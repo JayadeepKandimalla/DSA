354dacb69cd410b712746af0d298c3cb6cb1dce7
JayadeepKandimalla <jayadeep0001@gmail.com>
2026-07-08 23:54:41 +0530

    Time: 1 ms (78.23%), Space: 42.4 MB (51.35%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = helper(slow);
+            fast = helper(helper(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+        
+    }
+
+    private int helper(int n){
+        int sum= 0;
+        while(n>0){
+            int rem= n%10;
+            sum +=rem*rem;
+            n= n/10;
+        }
+        return sum;
+    }
+}

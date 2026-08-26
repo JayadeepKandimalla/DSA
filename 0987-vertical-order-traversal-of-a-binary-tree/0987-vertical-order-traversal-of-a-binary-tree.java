02eebdeb63387ab5e3d72b2f8dda38ad67c23ae2
Jayadeep Kandimalla <jayadeep0001@gmail.com>
2026-08-26 11:27:11 +0530

    Time: 3 ms (95.12%), Space: 44 MB (66.7%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -13,38 +13,39 @@
  *     }
  * }
  */
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        
-        List<int[]> nodes = new ArrayList<>();
+        List<int[]> nodes= new ArrayList<>();
         dfs(root, 0, 0, nodes);
 
-        nodes.sort((a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            if (a[1] != b[1]) return a[1] - b[1];
+        Collections.sort(nodes, (a,b) -> {
+            if(a[0]!=b[0])
+             return a[0] - b[0];
+            if(a[1]!=b[1])
+             return a[1] - b[1];
             return a[2] - b[2];
         });
 
-        List<List<Integer>> res = new ArrayList<>();
-        int prevCol = Integer.MIN_VALUE;
-        for (int[] node : nodes) {
-            int col = node[0], val = node[2];
-            if (col != prevCol) {
+        List<List<Integer>> res= new ArrayList<>();
+        int prev= Integer.MIN_VALUE;
+        for(int[] node: nodes){
+            int col= node[0], val= node[2];
+            if(prev!=col){
                 res.add(new ArrayList<>());
-                prevCol = col;
+                prev= col;
             }
-            res.get(res.size() - 1).add(val);
+
+            res.get(res.size()-1).add(val);
         }
 
         return res;
     }
 
-    private void dfs(TreeNode node, int col, int row, List<int[]> nodes) {
-        if (node == null) return;
-        nodes.add(new int[]{col, row, node.val});
-        dfs(node.left, col - 1, row + 1, nodes);
-        dfs(node.right, col + 1, row + 1, nodes);
+    public void dfs(TreeNode root, int row, int col, List<int[]> nodes){
+        if(root==null) return;
+
+        nodes.add(new int[]{col, row, root.val});
+        dfs(root.left, row+1, col-1, nodes);
+        dfs(root.right, row+1, col+1, nodes);
     }
 }
